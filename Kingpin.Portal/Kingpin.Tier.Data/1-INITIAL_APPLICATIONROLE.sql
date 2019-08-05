7d709b7f757cc0aeb56708356fa9f1766cb70101
Javier Pardo <javierpardollama@outlook.com>
2019-08-05 20:33:28 +0200

    FICHEROS DE INICIALIZACION DE DATOS

--- Kingpin.Portal/Kingpin.Tier.Data/1-INITIAL_APPLICATIONROLE.sql
+++ Kingpin.Portal/Kingpin.Tier.Data/1-INITIAL_APPLICATIONROLE.sql
@@ -1,5 +1,82 @@
 DELETE FROM ApplicationRole;
+
 delete from sqlite_sequence where name='ApplicationRole';
-INSERT INTO ApplicationRole (LASTMODIFIED,NAME,NORMALIZEDNAME,IMAGEURI,CONCURRENCYSTAMP) VALUES(date('now'),"User","User","roles\User_600px.png");
-INSERT INTO ApplicationRole (LASTMODIFIED,NAME,NORMALIZEDNAME,IMAGEURI,CONCURRENCYSTAMP) VALUES(date('now'),"Guest","Guest","roles\Guest_600px.png");
-INSERT INTO ApplicationRole (LASTMODIFIED,NAME,NORMALIZEDNAME,IMAGEURI,CONCURRENCYSTAMP) VALUES(date('now'),"Administrator","Administrator","roles\Administrator_600px.png");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Super Admin",
+"Super Admin",
+"roles\Super_Administrator_600px.png",
+"0x0000AAA0008E37A0");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Administrator",
+"Administrator",
+"roles\Administrator_600px.png",
+"0x0000AAA0008E37A0");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Editor",
+"Editor",
+"roles\Editor_600px.png",
+"0x0000AAA0008E37A0");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Author",
+"Author",
+"roles\Author_600px.png",
+"0x0000AAA0008E37A0");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Contributor",
+"Contributor",
+"roles\Author_600px.png",
+"0x0000AAA0008E37A0");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Subscriber",
+"Subscriber",
+"roles\Subscriber_600px.png",
+"0x0000AAA0008E37A0");
+
+INSERT INTO ApplicationRole (LASTMODIFIED,
+NAME,
+NORMALIZEDNAME,
+IMAGEURI,
+CONCURRENCYSTAMP) 
+VALUES(date('now'),
+"Guest",
+"Guest",
+"roles\Guest_600px.png",
+"0x0000AAA0008E37A0");
+
+
