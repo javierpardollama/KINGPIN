cbad8696769136f976354c02fdc5921210e5c82a
Javier Pardo <javierpardollama@outlook.com>
2019-10-27 20:02:05 +0100

    ACTUALIZACION DE PAQUETES NUGET

--- Kingpin.Portal/Kingpin.Tier.Web/Startup.cs
+++ Kingpin.Portal/Kingpin.Tier.Web/Startup.cs
@@ -102,7 +102,6 @@ namespace Kingpin.Tier.Web
             {
                 app.UseDeveloperExceptionPage();
                 app.UseCustomizedExceptionMiddlewares();
-
             }
             else
             {
