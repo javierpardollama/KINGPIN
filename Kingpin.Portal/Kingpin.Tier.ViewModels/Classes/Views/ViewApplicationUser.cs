0a39208974f002079d9e347df8d2cf3db291f1b2
Javier Pardo <javierpardollama@outlook.com>
2019-11-13 20:50:45 +0100

    ETIQUETAS DE SERIALIZACION DE XML

--- Kingpin.Portal/Kingpin.Tier.ViewModels/Classes/Views/ViewApplicationUser.cs
+++ Kingpin.Portal/Kingpin.Tier.ViewModels/Classes/Views/ViewApplicationUser.cs
@@ -1,31 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Serialization;
 
 using Kingpin.Tier.ViewModels.Interfaces.Views;
 
 namespace Kingpin.Tier.ViewModels.Classes.Views
 {
+    [XmlRoot("application-user")]
     public class ViewApplicationUser : IViewKey, IViewBase
     {
         public ViewApplicationUser()
         {
         }
 
+        [XmlElement("id")]
         public int Id { get; set; }
 
+        [XmlElement("last-modified")]
         public DateTime LastModified { get; set; }
 
+        [XmlElement("email")]
         public string Email { get; set; }
 
+        [XmlElement("initial")]
         public string Initial => Email.Substring(0, 1).ToUpper();
 
+        [XmlArray("application-user-roles")]
         public virtual ICollection<ViewApplicationUserRole> ApplicationUserRoles { get; set; }
 
+        [XmlArray("application-roles")]
         public virtual ICollection<ViewApplicationRole> ApplicationRoles => ApplicationUserRoles?.AsQueryable().Select(x => x.ApplicationRole).ToList();
 
+        [XmlArray("application-user-tokens")]
         public virtual ICollection<ViewApplicationUserToken> ApplicationUserTokens { get; set; }
 
+        [XmlElement("application-user-token")]
         public virtual ViewApplicationUserToken ApplicationUserToken => ApplicationUserTokens?.AsQueryable().LastOrDefault();
     }
 }
--- Kingpin.Portal/Kingpin.Tier.ViewModels/Classes/Views/ViewApplicationUserToken.cs
+++ Kingpin.Portal/Kingpin.Tier.ViewModels/Classes/Views/ViewApplicationUserToken.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Xml.Serialization;
 
 using Kingpin.Tier.ViewModels.Interfaces.Views;
 
 namespace Kingpin.Tier.ViewModels.Classes.Views
 {
+    [XmlRoot("application-user-token")]
     public class ViewApplicationUserToken : IViewBase
     {
         public ViewApplicationUserToken()
         {
         }
 
+        [XmlElement("last-modified")]
         public DateTime LastModified { get; set; }
 
+        [XmlElement("value")]
         public string Value { get; set; }
 
+        [XmlElement("application-user")]
         public virtual ViewApplicationUser ApplicationUser { get; set; }
     }
 }
