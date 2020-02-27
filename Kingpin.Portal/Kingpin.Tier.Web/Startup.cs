1e8537cbcc32f7cf3db1b3b9460c8f1849cefe79
Javier Pardo <javierpardollama@outlook.com>
2020-02-27 20:24:10 +0100

    LIMPIEZA DE CODIGO

--- Kingpin.Portal/Kingpin.Tier.Web/Startup.cs
+++ Kingpin.Portal/Kingpin.Tier.Web/Startup.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
